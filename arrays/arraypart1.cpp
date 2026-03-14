649bd4b172e9c19a546914623abd1c21c71336f7
swatirathore-lab <swatirathore683@gmail.com>
2026-03-14 09:22:11 +0530

    added array questions

--- arrays/arraypart1.cpp
+++ arrays/arraypart1.cpp
@@ -1,32 +1,13 @@
-// #include <iostream>
-// using namespace std;
-// int main() {
-//     int n;
-//     cout<<"enter length of array"<<endl;
-//     cin>>n;
-//     int arr[n];
-//     cout<<"enter elements of array"<<endl;
-//     for (int i=0;i<n;i++){
-//         cin>> arr[i];
-//     }
-//     for(int i=0;i<n;i++){
-//         cout<<arr[i]<<" ";
-//     }
-//     cout<<endl;
-//     return 0;
-// }
-// find largest in array
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n;
-    cout<<"enter length of array"<<endl;
-    cin>>n;
-    int arr[n];
-    cout<<"enter elements of array"<<endl;
-    for (int i=0;i<n;i++){
-        cin>> arr[i];
+int printArray(int arr[], int n) {
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
     }
+    cout<<endl;
+}
+//find largest in array
+int findLargest(int arr[], int n) {
     int largest = arr[0];
     for(int i=1;i<n;i++){
         if(arr[i]>largest){
@@ -34,5 +15,56 @@ int main(){
         }
     }
     cout<<"largest element in array is "<<largest<<endl;
+}
+//linear search in array
+int linearSearch(int *arr, int n, int key) {
+    for(int i=0; i<n; i++){
+        if(arr[i] == key){
+            return i;
+        }
+    }
+    return -1;
+    
+    
+}
+
+void reverseinarrayusingspace(int arr[], int n) {
+    int copyarr[n];
+    for(int i=0; i<n; i++){
+        copyarr[i] = arr[n-i-1];
+    }
+    cout << "reversed array: ";
+    for(int i=0; i<n; i++){
+        cout << copyarr[i] << " ";
+    }
+    cout << endl;
+    
+    
+}
+
+int main() {
+    int n, key;
+    cout<<"enter the length of array"<<endl;
+    cin>>n;
+    int start=0, end=n-1,temp;
+    int arr[n];
+    cout<<"enter elements of array"<<endl;
+    for (int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    printArray(arr, n);
+    findLargest(arr, n);
+    cout<<"enter the element to search"<<endl;
+    cin>>key;
+
+    int index= linearSearch(arr,n,key);
+    if(index==-1){
+        cout<<"element not found in array"<<endl;
+    }
+    else{
+        cout<<"element found at index "<<index<<endl;
+    }
+    reverseinarrayusingspace(arr, n);
+    
     return 0;
 }

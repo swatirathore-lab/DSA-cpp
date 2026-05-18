2732f761da5f0a618bd5d59a75601b8285cf2ac0
swatirathore-lab <swatirathore683@gmail.com>
2026-05-18 06:36:23 +0530

    adding vector questions

--- c++/Assignment Questions/vectors/vectors.cpp
+++ c++/Assignment Questions/vectors/vectors.cpp
@@ -44,10 +44,58 @@ void twoDdynamicarrays() {
     }
     delete[] arr; // deallocate memory for rows
 }
+void vectorDemo() {
+    vector<int> v(10, -1); // create a vector of 10 integers, all initialized to -1
+    cout<<v.size()<<"\n"; // print the size of the vector
+    cout << "Vector elements: ";
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " "; // access elements using index
+    }
+    cout << endl;
+}
+void vectorimplemenatationinmemory() {
+    vector<int> v={1,2,3,4,};
+    cout<<"size of vector is "<<v.size()<<endl;// size of vector is 4
+    cout<<"capacity of vector is "<<v.capacity()<<endl;// capacity of vector is 4
+    v.push_back(5);
+    cout<<"size of vector is "<<v.size()<<endl;// size of vector is 5
+    cout<<"capacity of vector is "<<v.capacity()<<endl;// capacity of vector is 8
+}
+vector<int> pairSum(vector<int> arr,int target){
+    int st=0, end=arr.size()-1;
+    int currSum=0;
+    while(st<end){
+        currSum=arr[st]+arr[end];
+        if(currSum==target){
+            return {st,end};
+        }
+        else if(currSum>target){
+            end--;
+        }
+        else{
+            st++;
+        }
+    }
+}
+void twoDvector(){
+    vector<vector<int>> matrix={{1,2,3},{4,5,6},{7,8,9}}; // create a 2D vector of 3 rows and 4 columns, all initialized to -1
+    cout<<"2D Vector elements: "<<endl;
+    for(int i=0;i<matrix.size();i++){
+        for(int j=0;j<matrix[i].size();j++){
+            cout<<matrix[i][j]<<" "; // access elements using index
+        }
+        cout<<endl;
+    }
+}
 int main() {
     function();
     twoDdynamicarrays();
+    vectorDemo();
+    vectorimplemenatationinmemory();
+    pairSum({1,2,3,4,5}, 5);
     return 0;
-
 }
+    
+
+
 

fc3eca4a7d4b5effcc4dbfb13f31d7e0aa1cf18e
swatirathore-lab <swatirathore683@gmail.com>
2026-05-29 15:03:04 +0530

    adding recurrsion questions

---
+++ c++/Assignment Questions/recurrsion/recurrsionpart1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Recursion: a function calling itself.
+
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num = 5;
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    return 0;
+}
+int factorial(int n){
+
+}

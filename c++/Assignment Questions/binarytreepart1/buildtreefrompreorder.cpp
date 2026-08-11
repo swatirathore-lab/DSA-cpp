c02c8f2239ec04970959bff84bfa4ac3cad1d8a4
swatirathore-lab <swatirathore683@gmail.com>
2026-08-12 01:21:34 +0530

    build tree from preorder

---
+++ c++/Assignment Questions/binarytreepart1/buildtreefrompreorder.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include <vector>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+    node(int data){
+        this->data=data;
+        left=right=NULL;
+    }
+    
+
+};
+static int idx=-1;
+node* build(vector<int> nodes){
+        
+    idx++;
+    if(nodes[idx]==-1){
+        return NULL;
+    }
+        
+    node* currnode=new node{nodes[idx]};
+    currnode->left=build(nodes);
+    currnode->right=build(nodes);
+    return currnode;
+}
+int main(){
+    vector<int> nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    node* root=build(nodes);
+    cout<<"root = "<<root->data<<endl;
+    return 0;
+}
